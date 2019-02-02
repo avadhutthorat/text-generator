4498517e0fd4c360f66f54ea4fbccd67b1f1a85c
avadhutthorat <avadhutthorat40@gmail.com>
2019-02-02 16:06:28 +0530

    Updates

---
+++ precache-manifest.4e97ab3cc3fa1b6efc40d2fc7d47182a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc27dcd261954ddbf0bd",
+    "url": "/text-generator/static/css/main.12e3b85f.chunk.css"
+  },
+  {
+    "revision": "fc27dcd261954ddbf0bd",
+    "url": "/text-generator/static/js/main.fc27dcd2.chunk.js"
+  },
+  {
+    "revision": "b3a89d6947bbf1f39e27",
+    "url": "/text-generator/static/js/1.b3a89d69.chunk.js"
+  },
+  {
+    "revision": "02237fbf25f43642e02c",
+    "url": "/text-generator/static/js/runtime~main.02237fbf.js"
+  },
+  {
+    "revision": "f219a20535c99d5dc873489977749bd9",
+    "url": "/text-generator/index.html"
+  }
+];
