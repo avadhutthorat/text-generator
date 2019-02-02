722a6c8a9a50511bdc7ba283f65a19f7c7ba3706
avadhutthorat <avadhutthorat40@gmail.com>
2019-02-02 16:09:59 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <h1 className="text-center display-3">Text Generator</h1>
+        <h1 className="text-center">Text Generator</h1>
         <hr />
         <form className="form-inline">
           <div className="form-group">
